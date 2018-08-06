c4d33407990ee8df87b563eb1c65c0e8f95a7323
Diego Garcia <qtimpot@gmail.com>
2018-08-06 11:17:46 -0400

    Build the app as web based

---
+++ server.py
@@ -0,0 +1,41 @@
+from app.app_state import AppState
+from app.classifier_controller import ClassifierController
+from flask import Flask
+from flask import render_template
+from flask import url_for
+from flask import request
+
+
+app = Flask(__name__)
+app_state = AppState()
+classifier_ctrl = ClassifierController(app_state)
+
+
+@app.route('/')
+@app.route('/classify', methods=['GET'])
+def render_classify():
+    return render_template('classifier.html', instance_names=app_state.get_instance_names())
+
+
+@app.route('/train')
+def render_train():
+    return render_template('train.html')
+
+
+@app.route('/classify', methods=['POST'])
+def classify_comment():
+    comment = request.form['comment']
+    instance_name = request.form['instance_name']
+    result = 'Empty comment'
+    if len(comment) > 0:
+        result = classifier_ctrl.classify(instance_name, comment)
+    return render_template('result.html', result=result)
+
+
+@app.route('train', methods=['POST'])
+def train_instance():
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
