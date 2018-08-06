9199e4dbefa1c3c3661c8c1d5c7e70cadc820091
Diego Garcia <qtimpot@gmail.com>
2018-08-06 11:08:27 -0400

    Format data processor

--- lib/data_processor.py
+++ lib/data_processor.py
@@ -6,11 +6,12 @@ import numpy as np
 
 
 class DataProcessor(object):
-    
+
     @staticmethod
     def create_1hot_vectors(values, possible_values):
         (num_values,) = values.shape
-        onehot_values = np.zeros((num_values, len(possible_values)), dtype=np.float32)
+        onehot_values = np.zeros(
+            (num_values, len(possible_values)), dtype=np.float32)
         for index in range(num_values):
             onehot_values[index, possible_values.index(values[index])] = 1
         return onehot_values
@@ -19,14 +20,18 @@ class DataProcessor(object):
     def clean_word(dirty_word):
         return re.sub(r'[\.,\-"\{\}\[\]\*\^;%\+&°!¡¿?#<>@/\(\)\\=:_~\$€\|]', '', dirty_word.lower())
 
-    def __init__(self, word_to_vector, sentence_length, channels):
+    def __init__(self,
+                 word_to_vector,
+                 sentence_length,
+                 channels):
         self.word_to_vector = word_to_vector
         self.sentence_length = sentence_length
         _, self.word_dimension = word_to_vector.vectors.shape
         self.channels = channels
 
     def process_one(self, sentence):
-        sample = np.zeros((self.sentence_length, self.word_dimension, self.channels))
+        sample = np.zeros(
+            (self.sentence_length, self.word_dimension, self.channels))
         word_index = 0
         for word in sentence.split():
             word = self.clean_word(word)
@@ -36,14 +41,15 @@ class DataProcessor(object):
                 word_embedding = self.word_to_vector[word]
             except:
                 continue
-            sample[word_index] = word_embedding.reshape((self.word_dimension, self.channels))
+            sample[word_index] = word_embedding.reshape(
+                (self.word_dimension, self.channels))
             word_index += 1
         return sample
 
     def process(self, sentences):
         (sentences_count,) = sentences.shape
-        samples = np.empty((sentences_count, self.sentence_length, self.word_dimension, self.channels), dtype=np.float32)
+        samples = np.empty((sentences_count, self.sentence_length,
+                            self.word_dimension, self.channels), dtype=np.float32)
         for sample_index in range(sentences_count):
             samples[sample_index] = self.process_one(sentences[sample_index])
         return samples
-
