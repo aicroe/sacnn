b4d401c53f07879bc38337ff2b775d4d779554c1
Diego Garcia <qtimpot@gmail.com>
2018-11-15 09:42:12 -0400

    Change reduce_labels script to ensure balance between new labels

--- reduce_labels.py
+++ reduce_labels.py
@@ -2,53 +2,63 @@ import numpy as np
 from os import sys
 from lib.data_saver import DataSaver
 
-if __name__ == '__main__':
-    current_data_num_labels = 5
-    new_num_labels = 3
 
-    if len(sys.argv) > 1 and int(sys.argv[1]) < current_data_num_labels:
-        new_num_labels = int(sys.argv[1])
+def reduce_set(dataset, labels):
+    assert dataset.shape[0] == labels.shape[0]
+
+    positives = labels[:, 0] == 1
+    neutrals = labels[:, 2] == 1
+    negatives = labels[:, 4] == 1
+
+    new_labels = np.concatenate((
+        labels[positives][:, 0:3],
+        labels[neutrals][:, 1:4],
+        labels[negatives][:, 2:5],
+    ), axis=0)
+
+    new_dataset = np.concatenate((
+        dataset[positives],
+        dataset[neutrals],
+        dataset[negatives],
+    ), axis=0)
+
+    new_indices = np.random.permutation(new_dataset.shape[0])
+    np.take(new_labels, new_indices, axis=0, out=new_labels)
+    np.take(new_dataset, new_indices, axis=0, out=new_dataset)
+
+    assert new_dataset.shape[0] == new_labels.shape[0]
+    return new_dataset, new_labels
+
+
+if __name__ == '__main__':
 
     (train_dataset,
-    train_labels,
-    val_dataset,
-    val_labels,
-    test_dataset,
-    test_labels) = DataSaver.load_data()
+     train_labels,
+     val_dataset,
+     val_labels,
+     test_dataset,
+     test_labels) = DataSaver.load_data()
 
-    new_train_labels = np.zeros((train_labels.shape[0], new_num_labels))
-    new_val_labels = np.zeros((val_labels.shape[0], new_num_labels))
-    new_test_labels = np.zeros((test_labels.shape[0], new_num_labels))
+    new_train_dataset, new_train_labels = reduce_set(train_dataset, train_labels)
+    new_val_dataset, new_val_labels = reduce_set(val_dataset, val_labels)
+    new_test_dataset, new_test_labels = reduce_set(test_dataset, test_labels)
 
-    new_train_labels[:, 0] = train_labels[:, 0] + train_labels[:, 1]
-    new_train_labels[:, 1] = train_labels[:, 2]
-    new_train_labels[:, 2] = train_labels[:, 3] + train_labels[:, 4]
-
-    new_val_labels[:, 0] = val_labels[:, 0] + val_labels[:, 1]
-    new_val_labels[:, 1] = val_labels[:, 2]
-    new_val_labels[:, 2] = val_labels[:, 3] + val_labels[:, 4]
-
-    new_test_labels[:, 0] = test_labels[:, 0] + test_labels[:, 1]
-    new_test_labels[:, 1] = test_labels[:, 2]
-    new_test_labels[:, 2] = test_labels[:, 3] + test_labels[:, 4]
-
-
-    print('train dataset shape: ({}, {}, {}, {})'.format(*train_dataset.shape))
+    print('train dataset shape: ({}, {}, {}, {})'.format(*new_train_dataset.shape))
     print('train labels shape: ({}, {})'.format(*new_train_labels.shape))
-    print('val dataset shape: ({}, {}, {}, {})'.format(*val_dataset.shape))
+    print('val dataset shape: ({}, {}, {}, {})'.format(*new_val_dataset.shape))
     print('val labels shape: ({}, {})'.format(*new_val_labels.shape))
-    print('test dataset shape: ({}, {}, {}, {})'.format(*test_dataset.shape))
+    print('test dataset shape: ({}, {}, {}, {})'.format(*new_test_dataset.shape))
     print('test labels shape: ({}, {})'.format(*new_test_labels.shape))
 
-    print('Data ratings : ' + str(list(range(1, new_num_labels + 1))))
+    print('Data ratings : ' + str(list(range(1, 4))))
     print('------ train : ' + str(np.sum(new_train_labels, axis=0)))
     print('------ val   : ' + str(np.sum(new_val_labels, axis=0)))
     print('------ test  : ' + str(np.sum(new_test_labels, axis=0)))
 
     DataSaver.save_reduced_data(
-        train_dataset,
+        new_train_dataset,
         new_train_labels,
-        val_dataset,
+        new_val_dataset,
         new_val_labels,
-        test_dataset,
+        new_test_dataset,
         new_test_labels)
